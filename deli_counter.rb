65aeea34557a6f12da2fb975786938c4abdb2dd6
Dominic DeRosa <dderosa71@gmail.com>
2021-08-31 16:21:04 -0500

    commit tree a44d4d7ee0b912e5ab23a00d508c3a3af3dbeb29

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+
+# def line(array)
+#     deliver_me = "The line is currently: "
+#     if array.length == 0
+#          puts "The line is currently empty."
+#     else
+#         array.each_with_index do |item, index|
+#             deliver_me << "#{index+1}. item "
+#         end
+#         puts deliver_me  
+#     end
+# end
+
+def line(arr)
+    line = []
+    if arr.length == 0
+        puts "The line is currently empty."
+    else 
+        line << arr.map.with_index do |customer, index|
+            "#{index+1}. #{customer}"
+        end
+    puts "The line is currently: #{line.join(" ")}"
+    end
+end
+
+
+
+
+
+
+    "The line is currently: 1. Ada 2. Grace 3. Kent"
+
+    "The line is currently empty.".
